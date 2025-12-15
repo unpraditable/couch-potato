84727ab38b34619280fb36fd7c94f520f757156c
Aditya Pradita <pradita397@gmail.com>
2025-12-16 00:16:31 +0700

    Add key to movie card and movie backdrop card to identify list items that are added, removed, or edited

--- src/components/Movie/MovieCard.tsx
+++ src/components/Movie/MovieCard.tsx
@@ -10,7 +10,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
     : "https://blocks.astratic.com/img/general-img-landscape.png";
   return (
-    <li className="grow-0 shrink-0 basis-auto flex-initial w-50 mb-8">
+    <li
+      key={movie.id}
+      className="grow-0 shrink-0 basis-auto flex-initial w-50 mb-8"
+    >
       <Link to={{ pathname: `/movie/${movie.id}` }}>
         <img
           className="rounded object-cover w-full h-75"
